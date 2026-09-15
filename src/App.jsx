c8465a0454c8f4eac57a54e552d0ec45d5663f08
ThienBao <thienbaomk147@gmail.com>
2026-09-15 19:25:06 +0700

    feat: git init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <div>Hello world</div>
+      <i className="fa-solid fa-house"></i>
+      <i className="fa-solid fa-user"></i>
+      <i className="fa-solid fa-cart-shopping"></i>
+    </>
+  );
+};
+
+export default App;
